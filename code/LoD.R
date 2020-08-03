c1afc8968f2f54d66c5f77f26eef78cc5161807b
Tengyao Wang <wangtengyao@gmail.com>
2020-08-04 00:09:25 +0100

    add helper function

--- code/LoD.R
+++ code/LoD.R
@@ -1,4 +1,5 @@
-# data matrix
+# data matrix, with three columns containing input copies,
+# number of positive reactions and number of negative reactions
 dat_mx <- matrix(c(10, 4, 8,
                    50, 11, 1 ,
                    100, 6, 0), ncol=3, byrow=TRUE)
@@ -14,14 +15,18 @@ loglikelihood <- function(p){
 # maximise likelihood
 ret <- optimise(loglikelihood, c(0,1), maximum=TRUE)
 p <- ret$maximum
-# sd=1/sqrt(-hessian(loglikelihood, p,method.args=list(eps=1e-8)))
+
 ps = seq(0,1,by=0.0001)
 loglik <- sapply(ps, loglikelihood)
-plot(ps, loglik, type='l') # visualise log likelihood
+setNA <- function (v, a) {v[is.na(v)] <- a; v}
 CI_p <- range(ps[setNA(loglik > ret$objective - qchisq(0.95,df=1) / 2, F)]) # confidence interval for p constructed using log likelihood ratio test (chisq with 1 dof under the null)
+
+plot(ps, loglik, type='l') # visualise log likelihood
+abline(v=p, lty=1, col='red')
+abline(v=CI_p, lty=2, col='red')
 
 # compute LoD and CI of LoD
 LoD <- -log(0.05)/p
 CI_LoD <- round(rev(-log(0.05)/CI_p), 1)
 
-println('LoD95 = ', round(LoD, 1), ', CI = (', CI_LoD[1], ', ', CI_LoD[2], ')')
+cat('LoD95 = ', round(LoD, 1), ', CI = (', CI_LoD[1], ', ', CI_LoD[2], ')', sep='')
