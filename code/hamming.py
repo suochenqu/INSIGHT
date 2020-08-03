0529480b7d392620e00a1422900fe211164f062e
suochenqu <55588895+suochenqu@users.noreply.github.com>
2020-08-04 00:37:49 +0100

    Update hamming.py

--- code/hamming.py
+++ code/hamming.py
@@ -10,22 +10,6 @@ import numpy as np
 Auxiliary functions and class 
 """
 
-# hamming distance between two strings of equal length
-def hamming_distance(string1, string2): 
-    return sum([string1[i] != string2[i] for i in range(len(string1))])
-
-# compute hamming ball of radius 2 around a barcode
-def hamming_ball(word):
-    n = len(word)
-    ball = set()
-    for i in range(n-1):
-        for j in range(i+1, n):
-            for c1 in 'ACGT':
-                for c2 in 'ACGT':
-                    tmp = word[:i] + c1 + word[(i+1):j] + c2 + word[(j+1):]
-                    ball.add(tmp)
-    return ball
-
 # Nucleotide is a class that represents the four nucleotides 'A' 'C' 'G' 'T'
 # as the field with four elements. Specifically, 'A' is the additive identity,
 # 'C' the multiplicative identity.
@@ -59,47 +43,13 @@ def show(mx):
     if len(mx.shape) == 2: # if mx is a matrix
         return [[letter.ind for letter in row] for row in mx]
     else: # if mx is a vector
-        return [letter.ind for letter in mx]
+        return [letter.ind for letter in mx] 
 
 
 #%%
 """
-Method 1: Generated barcodes with pairwise hamming distance at least 3
-
-Pick random unused barcode that does not lie in any hamming balls
-of radius 2 from previously selected barcodes. This can achieve the Gilbert-
-Varshamov lower bound of 4^n / (1 + 3n + 9n(n-1)/2). 
-"""
-def greedy_generation(n):
-    all_words = ['']
-    for i in range(n):
-        all_words = [word + c for word in all_words for c in 'ACGT']
-    unused = set()
-    codes = set()
-    
-    # add all words to the set 'unused', for faster deletion operation
-    for word in all_words:
-        unused.add(word)
-        
-    while len(unused) > 0:
-        next_word = unused.pop()
-        codes.add(next_word)
-        for word in hamming_ball(next_word):
-            if word in unused:
-                unused.remove(word)
-    
-    return codes 
-
-codes = greedy_generation(10)
-print(len(codes))  # 10958
-
-codes = greedy_generation(12)
-print(len(codes))  # 132832
-
-
-#%%
-"""
-Method 2: generate a linear code of code-length n. 
+Generate a linear code of code-length n.
+(for left and right barcodes each of length 5, take n = 10)
 
 First generate a parity matrix and the corresponding generator matrix. Then
 generate barcodes as the linear span of rows of the generator matrix. This 
@@ -142,9 +92,9 @@ def linear_generation(n):
     
     return(codes_str)
 
-codes = linear_generation(10) 
+codes = linear_generation(10) # `codes` contains 5-nt left and right barcode pairs
 len(codes)  # 16384
-codes = linear_generation(12)
+codes = linear_generation(12) # `codes` contains 6-nt left and right barcode pairs
 len(codes)  # 262144
 
 
