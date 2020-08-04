24d8c76b956211238e4d4acbbee1a4925abd3caf
suochenqu <55588895+suochenqu@users.noreply.github.com>
2020-08-04 10:37:01 +0100

    Update LoD.R

--- code/LoD.R
+++ code/LoD.R
@@ -8,25 +8,26 @@ dat_mx
 
 # loglikelihood function
 loglikelihood <- function(p){
-  prob_fail <- exp(-dat_mx[,1]*p)
-  sum(log(prob_fail)*dat_mx[,'fail'] + log(1-prob_fail)*dat_mx[,'success'])
+  prob_fail <- exp(-dat_mx[,1] * p)
+  sum(log(prob_fail) * dat_mx[,'fail'] + log(1 - prob_fail) * dat_mx[, 'success'])
 }
 
-# maximise likelihood
+# maximise likelihood estimation of p, the probability that a single molecule can be amplified
 ret <- optimise(loglikelihood, c(0,1), maximum=TRUE)
 p <- ret$maximum
 
 ps = seq(0,1,by=0.0001)
 loglik <- sapply(ps, loglikelihood)
 setNA <- function (v, a) {v[is.na(v)] <- a; v}
-CI_p <- range(ps[setNA(loglik > ret$objective - qchisq(0.95,df=1) / 2, F)]) # confidence interval for p constructed using log likelihood ratio test (chisq with 1 dof under the null)
+# confidence interval for p constructed using log likelihood ratio test (chisq with 1 dof under the null)
+CI_p <- range(ps[setNA(loglik > ret$objective - qchisq(0.95, df=1) / 2, FALSE)]) 
 
 plot(ps, loglik, type='l') # visualise log likelihood
-abline(v=p, lty=1, col='red')
-abline(v=CI_p, lty=2, col='red')
+abline(v=p, lty=1, col='red') # MLE
+abline(v=CI_p, lty=2, col='red') # plot confidence interval for p
 
-# compute LoD and CI of LoD
-LoD <- -log(0.05)/p
+# use MLE and CI of p to compute MLE and CI of LoD-95
+LoD <- round(-log(0.05)/p, 1)
 CI_LoD <- round(rev(-log(0.05)/CI_p), 1)
 
-cat('LoD95 = ', round(LoD, 1), ', CI = (', CI_LoD[1], ', ', CI_LoD[2], ')', sep='')
+cat('LoD95 = ', LoD, ', CI = (', CI_LoD[1], ', ', CI_LoD[2], ')\n', sep='')
